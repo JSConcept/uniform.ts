69aeec931483cee0ee00dd729bf639ac2b433e09
unit-404 <conway24b@gmail.com>
2024-10-05 21:58:44 +0700

    cheepful

--- src/Handlers/UniversalHandler.ts
+++ src/Handlers/UniversalHandler.ts
@@ -18,19 +18,14 @@ export default class UniversalHandler extends DataHandler {
 
     //
     $handle(cmd = "access", t, ...args) {
-        const meta  = extract(t);
-        const local = this.$get(meta);
-
         //
         let htp = "direct";
         if (isPromise(t?.[$data] ?? t)) 
             { htp = "promise"; } else
-
-            if (typeof meta?.["@uuid"] == "string") {
-                if (local && extract(local)?.["@uuid"] != meta?.["@uuid"]) 
-                    { htp = "local"; } else 
-                if (meta?.["@uuid"]) 
-                    { htp = "remote"; }
+            {
+                const meta = extract(t), local = this.$get(meta);
+                if (typeof meta?.["@type"] == "string") { htp = "local"; }
+                if (typeof meta?.["@uuid"] == "string" && !(local && extract(local)?.["@uuid"] != meta?.["@uuid"])) { htp = "remote"; }
             }
 
         //
