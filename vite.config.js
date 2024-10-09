814cf25ea2ec559bd49a055fcfa1f4b742b3d01c
unit-404 <conway24b@gmail.com>
2024-10-09 08:58:18 +0700

    test

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig, loadEnv } from "vite";
 
+//
 export default defineConfig({
     server: {
         port: 5173,
+        open: false,
+        origin: "http://localhost:5173/",
+        
     },
 });
