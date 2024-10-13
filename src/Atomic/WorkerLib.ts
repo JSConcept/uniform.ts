4d17b50b5bdb0a8edd7b684cb2f83f589d6a98e6
unit-404 <conway24b@gmail.com>
2024-10-13 09:04:51 +0700

    determination of code

--- src/Atomic/WorkerLib.ts
+++ src/Atomic/WorkerLib.ts
@@ -4,19 +4,15 @@ export const loadWorker = (WX: any): Worker|null =>{
     if (typeof WX == "function") { return new WX(); } else 
     if (typeof WX == "string") { return new Worker(WX, {type: "module"}); }
     return null;
-
-    //
-    /**/
 }
 
 //
-export const loadWorkerCompressed = async (b64c: string): Promise<Worker|null> => {
+export const loadCompressed = async (b64c: string): Promise<string|null> => {
     const blob = new Blob([Uint8Array.from(atob(b64c), c => c.charCodeAt(0))], {type: "application/gzip"});
     const ds = new DecompressionStream("gzip");
     const decompressedStream = blob.stream().pipeThrough(ds);
     const response = await (new Response(decompressedStream, {headers: new Headers({"Content-Type": "application/javascript" })})).blob();
-    const url = URL.createObjectURL(response);
-    return new Worker(url, {type: "module"});
+    return URL.createObjectURL(response);
 }
 
 //
--- src/ESM/ModuleLoader.ts
+++ src/ESM/ModuleLoader.ts
@@ -1,18 +1,15 @@
+// deno-lint-ignore-file no-explicit-any
 // just compressed base64 encoded string, we currently doesn't supports native wrappers from custom vite
 // `inline` will also ignored in custom vite bundle, prior of `compress`
-import IW from "../Workers/ModuleWorker.ts?worker&compress&inline";
-
-// deno-lint-ignore-file
-import loadWorker, {loadWorkerCompressed} from "../Atomic/WorkerLib.ts";
-import ObjectProxy from "../Atomic/ObjectProxy.ts";
-// you needs custom Vite bundle to compile less sized bundles
+import $raw$ from "../Workers/ModuleWorker.ts?worker&compress&inline"; const IW = $raw$; // put into start of code
 
 //
+import loadWorker, {loadCompressed} from "../Atomic/WorkerLib.ts";
 const $moduleLoader = async <T extends unknown>(moduleSource: string): Promise<T> => {
     if (!moduleSource || typeof moduleSource != "string") throw new Error("Invalid module source");
 
     // if url too long, un-compress code
-    const  uWorker  = (IW as unknown as string)?.length >= 1024 ? (await loadWorkerCompressed(IW as unknown as string)) : loadWorker(IW);
+    const  uWorker  = loadWorker((IW as unknown as string)?.length >= 1024 ? (await loadCompressed(IW as unknown as string)) : IW);
     const EXChanger = (await import("../Library/FLOW/ExChanger.ts")).default;
     const exChanger = new EXChanger(uWorker)?.initialize?.();
     const module    = await (await exChanger?.access?.("!!import!!") as any)?.(moduleSource);
@@ -22,6 +19,7 @@ const $moduleLoader = async <T extends unknown>(moduleSource: string): Promise<T
 }
 
 //
+import ObjectProxy from "../Atomic/ObjectProxy.ts";
 import {MakeReference, PromiseHandler} from "../Library/Shared.ts";
 export const moduleLoader = <T extends unknown>(source: string)=>{
     return new Proxy(MakeReference($moduleLoader<T>(source)?.catch?.(console.trace.bind(console))), new ObjectProxy(new PromiseHandler()));
